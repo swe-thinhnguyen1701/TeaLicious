81bb79e7e8042038715770e4fafdee9559ad12d8
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-06 09:45:08 -0500

    set up server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
+const { resolvers, typeDefs } = require("./schemas");
+const cors = require("cors");
+const path = require("path");
+const db = require("./config/connection");
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startApolloServer = async () => {
+    await server.start();
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    app.use(cors());
+
+    app.use("/graphql", expressMiddleware(server, {
+        context: authMiddleware,
+    }));
+
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static(path.join(__dirname, "../client/dist")));
+
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+        });
+    }
+
+    db.once("open", () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at XXXXXXXXXXXXXXXX:${PORT}/graphql`);
+        });
+    });
+};
+
+startApolloServer();
