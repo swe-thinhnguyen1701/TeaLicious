8c0ae5501af26cabd1bd447635281cd6547a96cd
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-08 16:22:15 -0700

    modify the Header.jsx to make the Header reusable; modify HomePage.jsx and Navbar.jsx

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,35 @@
+import Header from "../../components/Header";
+import { Link } from "reactrouer-dom";
 import "./HomePage.css";
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <Header showAuthButtons={true} showCategories={true} />
+            <h1 className="title">TeaLicious</h1>
+            <div className="statement">Lorem ipsum</div>
+            <Link to="/Products">
+                <button className="button">Get started</button>
+            </Link>
+        </div>
+    );
+}
+
+export default HomePage;
+
+{/*THIS CODE SHOULD BE INSERTED IN THE PRODUCTS.JSX:
+import Header from "./Header";
+import Navbar from "./Navbar";
+import "./ProductsPage.css";
+
+function ProductsPage() {
+    return (
+        <div className="ProductsPage">
+            <Header showAuthButtons={false} showCategories={true} />
+            <Navbar />
+            
+        </div>
+    );
+}
+
+export default ProductsPage; */}
