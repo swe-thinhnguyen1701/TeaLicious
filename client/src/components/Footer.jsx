3654feec987ae142fe3e4014c385994c9f87cc99
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-08 14:26:25 -0700

    create folder components and five files: Button.jsx, Card.jsx,Footer.jsx, Header.jsx, Navbar.jsx; wwrite code for three components: Header.jsx, Navbar.jsx, Footer.jsx; create css files in folder pages

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <ul>
+                    <li className='signature'>Made by DreamTeam, 2024</li>     
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
