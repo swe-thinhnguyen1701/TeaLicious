474ccd7820c4f0af818caa1c598dfc7a4404cbb3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-10 12:03:39 -0500

    build category component

---
+++ client/src/components/CategoryList.jsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
+import {GET_CATEGORIES} from "../utils/queries";
+
+function CategoryList () {
+    const {loading, data} = useQuery(GET_CATEGORIES);
+
+    if(loading) return <h2>Loading</h2>;
+
+    return (
+        <div className="category-container">
+            {data.map(category => {
+                return (
+                    <div className="category" key={category._id}>
+                        <Link to={`/${category.name}`}>{category.name}</Link>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default CategoryList;
