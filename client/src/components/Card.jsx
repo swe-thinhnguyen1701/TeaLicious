c1db44dc4409047087dd9894dd4d3fba7f991951
marissamelo91 <marissa.melo91@yahoo.com>
2024-09-09 17:18:16 -0700

    worked on Card.jsx file

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Card() {
+  return (
+    <div className="card">
+      <img src="" alt=""></img>
+      <h2>Product Name</h2>
+      <p>Product Description</p>
+      <p>Price</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+
+}
+
+
+export default Card;
