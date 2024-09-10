9bf67afd54779aed721e2c0beab7ec5785a1548a
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-09 22:47:36 -0500

    update app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from "@apollo/client/link/context"
 import { Outlet } from 'react-router-dom';
 
-// import HomePage from './pages/HomePage/HomePage';
-// import ProductsPage from './pages/ProductsPage/ProductsPage'; //Marissa will create ProductsPage.jsx
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-// import Header from './components/Header';
-// import LoginPage from './pages/LoginPage/LoginPage'; // I will create LoginPage.jsx
-// import UserPage from './pages/UserPage/UserPage'; // I will create UserPage.jsx
 import './index.css';
 
 const httpLink = createHttpLink({
