97aab1537c4368dd8fbf930b6a1174abdcae5c8e
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-11 10:21:44 -0700

    modify style for the Home page: edit index.css, HomePage.jsx, HomePage.css files

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import "./HomePage.css";
 
 function HomePage() {
-    return (
+           return (
         <div className="HomePage">
            
             <h1 className="title">TeaLicious</h1>
-            <div className="statement">Lorem ipsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essen</div>
+            <div className="statement">Life is like a cup of tea. It's all about how you make it...</div>
             <Link to="/Products">
                 <button className="button">Get started</button>
             </Link>
