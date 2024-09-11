dd198ebab5362f6d3e835f134c9cf297e999f049
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-11 15:02:34 -0700

    add animation at the HomePage.jsx, update HomePage.css and style.css; update style of the UserPage: modify UserPage.jsx and UserPage.css

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -10,6 +10,29 @@ function HomePage() {
             <div className="home-page-statement">Lorem ipsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essen</div>
             <Link to="/products" className="link get-started-link">
             GET STARTED <FontAwesomeIcon icon={faChevronRight} className="icon" />
+    return (
+        <div className="HomePage">
+
+            <h1 className="title">TeaLicious</h1>
+            <div className="statement">
+                <span>Life </span>
+                <span>is </span> 
+                <span>like </span>
+                <span>a </span>
+                <span>cup </span> 
+                <span>of </span>
+                <span>tea. </span>
+                <span>It </span> 
+                <span>is </span>
+                <span>all </span> 
+                <span>about </span>
+                <span>how </span>
+                <span>you </span>
+                <span>make </span>
+                <span>it...</span>
+            </div>
+            <Link to="/Products">
+                <button className="button">GET STARTED</button>
             </Link>
         </div>
     );
