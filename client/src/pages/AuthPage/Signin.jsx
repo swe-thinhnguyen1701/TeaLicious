416e44a9e1801b0614f5f2a169ef749230edd7e1
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-11 15:56:35 -0500

    build sign up and sign in

--- client/src/pages/AuthPage/Signin.jsx
+++ client/src/pages/AuthPage/Signin.jsx
@@ -1,14 +1,50 @@
-function Signin () {
-    return(
-        <div className="form-container sign-in">
-        <form>
-          <h1>Sign In</h1>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button type="button">Sign In</button>
-        </form>
-      </div>
-    )
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../../utils/mutations"
+import Auth from '../../utils/auth';
+
+function Signin() {
+  const [userFormData, setUserFormData] = useState({ identifier: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+    }
+    setUserFormData({
+      identifier: "",
+      password: "",
+    });
+  };
+  return (
+    <div className="form-container sign-in">
+      <form onSubmit={handleFormSubmit}>
+        <h1>Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email or Username"
+          name="identifier"
+          onChange={handleInputChange}
+          required />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleInputChange}
+          required />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  )
 }
 
 export default Signin;
--- client/src/pages/AuthPage/Signup.jsx
+++ client/src/pages/AuthPage/Signup.jsx
@@ -1,14 +1,63 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import Auth from "../../utils/auth"
 import "./style.css";
 
 function Signup() {
+    const [userFormData, setUserFormData] = useState({ username: "", email: "", password: "" });
+
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
+
+    const handleSubmitForm = async (e) => {
+        e.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...userFormData },
+            });
+            Auth.login(data.addUser.token);
+        } catch (err) {
+            console.error(err);
+        }
+
+        setUserFormData({
+            username: "",
+            email: "",
+            password: "",
+        });
+    }
     return (
         <div className="form-container sign-up">
-            <form>
+            <form onSubmit={handleSubmitForm}>
                 <h1>Create Account</h1>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button type="button">Sign Up</button>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    onChange={handleInputChange}
+                    value={userFormData.username}
+                    required />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    onChange={handleInputChange}
+                    value={userFormData.email}
+                    required />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={handleInputChange}
+                    value={userFormData.password}
+                    required />
+                <button type="submit">Sign Up</button>
             </form>
         </div>
     );
