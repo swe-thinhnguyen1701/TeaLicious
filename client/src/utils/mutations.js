70af96340e0261bcf94e8647db278ce7bb715346
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-11 15:57:34 -0500

    fix login parameters

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -16,8 +16,8 @@ export const ADD_USER = gql`
 
 
 export const LOGIN = gql`
-  mutation login($username: String!, $email: String!, $password: String!) {
-    login(username: $username, email: $email, password: $password) {
+  mutation login($identifier: String!, $password: String!) {
+    login(username: $identifier, email: $identifier, password: $password) {
       token
       user {
         _id
