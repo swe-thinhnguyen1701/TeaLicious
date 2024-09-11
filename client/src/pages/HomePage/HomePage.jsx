896879e1a76025c0f2a4b1b1bb7d70b4f078e1d3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-11 15:56:56 -0500

    fix spelling

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -10,12 +10,6 @@ function HomePage() {
             <div className="home-page-statement">Lorem ipsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essen</div>
             <Link to="/products" className="link get-started-link">
             GET STARTED <FontAwesomeIcon icon={faChevronRight} className="icon" />
-    return (
-        <div className="home-page page">
-            <h1 className="home-page-title">TeaLicious</h1>
-            <div className="home-page-statement">Lorem ipsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essen</div>
-            <Link to="/Products" className="link get-started-link">
-            GET STARTED <FontAwesomeIcon icon={faChevronRight} className="icon" />
         <div className="HomePage">
 
             <h1 className="title">TeaLicious</h1>
