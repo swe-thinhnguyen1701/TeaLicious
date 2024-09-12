14cd501392859018c7589fb3c8228a692a8c431e
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-09-12 18:16:26 -0500

    build product detail component

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -1,24 +1,104 @@
-import React from "react";
-import { Link } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_PRODUCT } from "../utils/queries";
+import { ADD_ITEM_TO_CART } from "../utils/mutations";
+import "./style.css";
 
-function ProductsPage() {
+function ProductDetail({ productId }) {
+    const { loading, error, data } = useQuery(GET_PRODUCT, {
+        variables: { _id: productId }
+    });
+    const [addItemToCart] = useMutation(ADD_ITEM_TO_CART);
+    const [quantity, setQuantity] = useState(1);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>ERROR happens</p>
+
+    const product = data.getProduct;
+
+    const cartId = localStorage.getItem("cart_id");
+    // console.log(cartId);
+    
+
+    const quantityOnChange = (e) => {
+        if(e.target.value === ""){
+            setQuantity(0);
+            return;
+        }
+        setQuantity(parseInt(e.target.value));
+    }
+
+    const addItemHandler = () => {
+        if (quantity < product.stock) {
+            setQuantity(quantity + 1);
+        }
+    }
+
+    const reduceItemHandler = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    }
+
+    const addToCartHandler = async () => {
+        try {
+            console.log("cartID :>>", cartId);
+            console.log("product._id :>>", product._id);
+            console.log("quantity :>>", quantity);
+            
+            await addItemToCart({
+                variables: {
+                    _id: cartId,
+                    productId: product._id,
+                    quantity: quantity
+                }
+            });
+        } catch (error) {
+            console.error("ERROR occurs while adding ITEM to CART", error);
+            throw new Error("Failed to add item to cart");
+        }
+    }
+
     return (
-        <div className="card-container">
-            <img
-                className="card-img"
-                src=""
-                alt="Card Image">
-            </img>
-            <h2 className="card-title">Product Name</h2>
-            <p className="card-description">Product Description</p>
-            <p className="card-stock">Stock</p>
-            <p className="card-weight">weight</p>
-            <p className="card-price">Price</p>
-            <button className="add-cart" onClick={addToCart}>Add to Cart</button>
+        <div className="product-container">
+            <div className="product-img-container">
+                <img
+                    className="product-img"
+                    src={product.image}
+                    alt="Product Image">
+                </img>
+            </div>
+            <div className="product-info-container">
+                <div className="product-info">
+                    <h2 className="product-name">{product.name}</h2>
+                    <p className="product-description">{product.description}</p>
+                    <p className="product-stock">{product.stock}</p>
+                    <p className="product-weight">{product.weight}</p>
+                    <p className="product-price">${product.price}</p>
+                </div>
+                <div className="product-action">
+                    <div className="product-quantity">
+                        <button
+                            className="btn"
+                            onClick={reduceItemHandler}>-</button>
+                        <input
+                            className="input"
+                            type="number"
+                            value={quantity}
+                            min={1}
+                            onChange={quantityOnChange} />
+                        <button
+                            className="btn"
+                            onClick={addItemHandler}>+</button>
+                    </div>
+                    <button className="btn" onClick={addToCartHandler}>Add to Cart</button>
+                </div>
+            </div>
         </div>
     );
 
 }
 
 
-export default ProductsPage;git
+export default ProductDetail;
