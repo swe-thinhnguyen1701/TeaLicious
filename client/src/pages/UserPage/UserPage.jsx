452669c38ede58da10fe1a13aaebe38371cea6b4
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-09-13 21:50:34 -0700

    update userpage.jsx

--- client/src/pages/UserPage/UserPage.jsx
+++ client/src/pages/UserPage/UserPage.jsx
@@ -1,56 +1,23 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useQuery } from '@apollo/client';
 import { GET_ME } from '../../utils/queries';
 import './UserPage.css';
-
 const UserPage = () => {
-    const {loading, data, error} = useQuery(GET_ME);
-    // const user = data?.me || {}
-    const user = data?.me || {};
-    useEffect(() => {
-        console.log("loading:", loading);
-        console.log("error:", error);
-        console.log("data:", data);
-        if (error) {
-            console.error("ERROR :>>", error);
-        }
-    }, [data, error, loading]);
-    
-    const [userInfo, setUserInfo] = useState({
-        username: user.username,
-        email: user.email,
-        password: '',
-        address: {
-            street: user.address.street ? user.address.street : "",
-            city: user.address.city ? user.address.city : "",
-            state: user.address.state ? user.address.state : "",
-            zip: user.address.zip ? user.address.zip : ""
-        }
-    });
-
-    useEffect(() => {
-        hideData("username", user.username);
-        hideData("email", user.email);
-    }, [user]);
-
-    if (loading) return <p>Loading...</p>;
-
-    const hideData = (dataType, data) => {
-        if (dataType === "username")
-            userInfo.username = data.slice(0, 3) + "*******";
-        else {
-            const [emailUsername, emailDomain] = userInfo.email.split("@");
-            userInfo.email = emailUsername.slice(0, 3) + "********@" + emailDomain;
+    const {loading, data} = useQuery(GET_ME)
+    const hideData = (dataType, value) => {
+        if(!value) {
+            return "";
+        } else {
+            if (dataType === "username")
+                return value?.slice(0, 3) + "*******";
+            else {
+                const [emailUsername, emailDomain] = value?.split("@");
+                return emailUsername.slice(0, 3) + "********@" + emailDomain;
+            }
         }
     }
-
-    const handleUpdate = () => {
-        console.log('User info updated', userInfo);
-    };
-
     const handleChange = (e) => {
         const { name, value } = e.target;
-
         if (['street', 'city', 'state', 'zip'].includes(name)) {
             setUserInfo((prevState) => ({
                 ...prevState,
@@ -66,6 +33,17 @@ const UserPage = () => {
             });
         }
     };
+    const userInfo = {
+        username: hideData("username", data?.me.username),
+        email: hideData("email", data?.me.email),
+        // password: data?.me.password,
+        address: data?.me.address
+    }
+    if(loading) {
+        return (
+            <h1>Still loading, please wait!</h1>
+        )
+    }
     return (
         <div className="user-page page">
             <h1 className="profile">Profile</h1>
@@ -74,49 +52,53 @@ const UserPage = () => {
                     <label>Username:</label>
                     <input type="text"
                         name="username"
-                        value={userInfo.username} />
+                        value={userInfo.username}
+                        // onChange={handleChange}
+                        readOnly
+                        />
                 </div>
                 <div className="input-field">
                     <label>Email:</label>
                     <input type="text"
                         name="email"
-                        value={userInfo.email} />
+                        value={userInfo.email}
+                        // onChange={handleChange}
+                        readOnly
+                        />
                 </div>
-                <div className="input-field">
+                {/* <div className="input-field">
                     <label>Password:</label>
                     <input type="password"
                         name="password"
                         value={userInfo.password}
                         onChange={handleChange} />
-                </div>
+                </div> */}
                 <div className="input-field">
                     <label>Address:</label>
                     <input className="address-input" type="text"
                         name="street"
                         placeholder="Street"
-                        value={userInfo.address.street}
+                        value={userInfo.address?.street || ""}
                         onChange={handleChange} />
                     <input className="address-input" type="text"
                         name="city"
                         placeholder="City"
-                        value={userInfo.address.city}
+                        value={userInfo.address.city || ""}
                         onChange={handleChange} />
                     <input className="address-input" type="text"
                         name="state"
                         placeholder="State"
-                        value={userInfo.address.state}
+                        value={userInfo.address.state || ""}
                         onChange={handleChange} />
                     <input className="address-input" type="text"
                         name="zip code"
                         placeholder="Zip code"
-                        value={userInfo.address.zip}
+                        value={userInfo.address.zip || ""}
                         onChange={handleChange} />
                 </div>
-                <button className="update-button" onClick={handleUpdate}>UPDATE</button>
+                <button className="update-button" onClick={() => {}}>UPDATE</button>
             </div>
         </div>
     );
-
 };
-
 export default UserPage;
