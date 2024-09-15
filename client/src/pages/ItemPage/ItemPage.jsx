197e7ed57bc1761e40790ecb05e9d4eb49694fec
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-15 15:18:12 -0700

    update CSS for HomePage, UserPage, upload image for backgrounds

--- client/src/pages/ItemPage/ItemPage.jsx
+++ client/src/pages/ItemPage/ItemPage.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-undef */
 import ProductDetail from "../../components/ProductDetail";
 import { useParams } from "react-router-dom";
+import './ItemPage.css';
 
 function ItemPage() {
     const { id:productId } = useParams();
     return (
-        <div className="page">
+        <div className="item-page">
             <ProductDetail productId={productId} />
         </div>
     )
